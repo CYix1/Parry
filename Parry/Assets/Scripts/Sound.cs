adb4e595baa2925c82df527cb2543fbbff826a9e
CYix1 <yixuan@outlook.de>
2023-03-13 16:40:37 +0100

    audio stuff

---
+++ Parry/Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿    
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Audio;
+[System.Serializable]
+    public class Sound
+    {
+        public AudioClip clip;
+
+        public string name;
+        [Range(0f, 1f)] public float volume=1;
+        [Range(1f, 3f)] public float pitch;
+
+        public bool loop;
+        
+    }
