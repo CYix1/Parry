77adc7981ba47008e39e5e5c842486d7d188cab6
CYix1 <yixuan@outlook.de>
2023-03-16 15:50:00 +0100

    powerup

---
+++ Parry/Assets/Scripts/LevelGeneration/PowerupSpawner.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using LevelGeneration;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEditor;
+using Random = UnityEngine.Random;
+
+public class PowerupSpawner : MonoBehaviour
+{
+    public Transform[] positions;
+    public GameObject[] powerups;
+    public float spawnintervall=5f;
+    [SerializeField]private float timer;
+
+    public void SpawnPowerup()
+    {
+        Debug.Log("SPAWENOIAENFAs");
+        var pw = powerups[Random.Range(0, powerups.Length)];
+        var new_pw=Instantiate(pw.transform, positions[Random.Range(0,positions.Length)].position,Quaternion.identity);
+        new_pw.AddComponent<MovingObject>();
+        new_pw.gameObject.SetActive(true);
+    }
+
+
+    private void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer > spawnintervall)
+        {   
+            SpawnPowerup();
+            timer = 0;
+        }
+    }
+}
---
+++ Parry/Assets/Scripts/Powerup/ShieldPowerup.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class ShieldPowerup: MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject shield;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            
+            Debug.Log("dao");
+        }
+    }
+}
