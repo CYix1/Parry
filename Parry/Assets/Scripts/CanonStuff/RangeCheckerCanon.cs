51758323de10d8bad944f5d3f8690cb8d332a5bd
Kerstin Pfaffinger <kerstin.pfaffinger@tum.de>
2023-03-16 10:39:48 +0100

    adjusted canon prefab and removed collider customization from its script

--- Parry/Assets/Scripts/CanonStuff/RangeCheckerCanon.cs
+++ Parry/Assets/Scripts/CanonStuff/RangeCheckerCanon.cs
@@ -1,47 +1,33 @@
 using UnityEngine;
 
-// checks if player is in range and shoots only if yes
+// checks if player is in range and shoots only if so
 namespace CanonStuff
 {
     public class RangeCheckerCanon : MonoBehaviour
     {
-        [SerializeField] private float laneWidth = 9;
-        [SerializeField] private float playerRange = 10;
-
         private BoxCollider _collider;
         private Canon _canon;
 
         private void Start()
         {
-            InitColliderSize();
             InitCanon();
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (!IsPlayer(other)) return;
-        
+
             _canon.StartShooting();
         }
-    
+
         private void OnTriggerExit(Collider other)
         {
             if (!IsPlayer(other)) return;
-        
+
             _canon.StopShooting();
         }
 
         #region Init
-
-        private void InitColliderSize()
-        {
-            _collider = GetComponent<BoxCollider>();
-
-            var scale = _collider.transform.localScale;
-            Vector3 laneSize = new Vector3(playerRange, 6, laneWidth);
-
-            _collider.transform.localScale = laneSize;
-        }
 
         private void InitCanon()
         {
